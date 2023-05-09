5f83b63fcaa53cad50abedf68bf8a51cf8170e19
Heytemisan <temyicy@gmail.com>
2023-05-09 10:07:49 +0100

    feat: authentication buildup

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { themeSettings } from "./theme";
 import NotFoundPage from "./scenes/404";
 
 const HomePage = lazy(() => import("./scenes/homePage"));
-const LoginPage = lazy(() => import("./scenes/authentication"));
+const LoginPage = lazy(() => import("./scenes/login"));
 const ProfilePage = lazy(() => import("./scenes/profilePage"));
 const SettingsPage = lazy(() => import("./scenes/settingsPage"));
 const EditSettings = lazy(() => import("./scenes/settingsPage/editSettings"));
